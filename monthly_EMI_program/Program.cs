2508a7a07e012c3e96b77ff37699552f88fdbe69
shamnan-18 <ahmadshamnan575@gmail.com>
2025-10-03 10:19:55 +0530

    initial commit

---
+++ monthly_EMI_program/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Monthly_EMI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Principal amount: ");
+            double principal = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the total years of tenure: ");
+            int years = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the annual interest rate (in %): ");
+            double annualRate = Convert.ToDouble(Console.ReadLine());
+
+            double monthlyRate = annualRate / (12 * 100);
+
+
+            int totalMonths = years * 12;
+
+
+            double emi;
+
+            if (monthlyRate == 0)
+            {
+                emi = principal / totalMonths;
+            }
+            else
+            {
+                double pow = Math.Pow(1 + monthlyRate, totalMonths);
+                emi = principal * monthlyRate * pow / (pow - 1);
+            }
+
+            Console.WriteLine("\nMonthly EMI is: ₹" + Math.Round(emi, 2));
+        }
+    }
+}
