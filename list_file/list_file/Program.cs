a44ab93a5b2dfcc072b192d4767f53255bfeb727
shamnan-18 <ahmadshamnan575@gmail.com>
2025-10-03 10:26:17 +0530

    initial commit

---
+++ list_file/list_file/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace ListFilesInDirectory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the directory path: ");
+            string dirPath = Console.ReadLine();
+
+            if (Directory.Exists(dirPath))
+            {
+                string[] files = Directory.GetFiles(dirPath);
+
+                Console.WriteLine($"\nFiles in directory: {dirPath}\n");
+
+                if (files.Length == 0)
+                {
+                    Console.WriteLine("No files found.");
+                }
+                else
+                {
+                    foreach (string file in files)
+                    {
+                        Console.WriteLine(Path.GetFileName(file));
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Directory does not exist.");
+            }
+        }
+    }
+}
