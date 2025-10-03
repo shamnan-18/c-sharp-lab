1c2deb551482d41646804f8ab076b8f22dc6f0ea
shamnan-18 <ahmadshamnan575@gmail.com>
2025-10-03 10:29:12 +0530

    initial commit

---
+++ currency_converter/currency_converter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CurrencyConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter amount in INR: ");
+            double inrAmount = Convert.ToDouble(Console.ReadLine());
+
+
+            double usdRate = 0.012;
+            double eurRate = 0.011;
+            double gbpRate = 0.0095;
+
+            double usd = inrAmount * usdRate;
+            double eur = inrAmount * eurRate;
+            double gbp = inrAmount * gbpRate;
+
+            Console.WriteLine($"\n{inrAmount} INR = {usd:F2} USD");
+            Console.WriteLine($"{inrAmount} INR = {eur:F2} EUR");
+            Console.WriteLine($"{inrAmount} INR = {gbp:F2} GBP");
+        }
+    }
+}
