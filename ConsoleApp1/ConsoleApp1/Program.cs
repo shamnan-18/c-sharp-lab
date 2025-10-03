b0a53214b65f58e6740362d341dbb41b73a870e3
shamnan-18 <ahmadshamnan575@gmail.com>
2025-10-03 10:16:41 +0530

    initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace payment
+{
+    interface Ipayment
+    {
+        public void Makepayment(double amount);
+    }
+    class Creditcard : Ipayment
+    {
+        public void Makepayment(double amount)
+        {
+            Console.WriteLine("INR {amount} done through credit card");
+        }
+    }
+    class Paypal:Ipayment
+    {
+        public void Makepayment(double amount)
+        {
+            Console.WriteLine(amount + "done through paypal");
+        }
+        class UPI:Ipayment
+        {
+            public void Makepayment(double amount)
+            {
+                Console.WriteLine("INR" + amount+ "payment thrugh UPI");
+            }
+        }
+        class Program
+        {
+            static void  Main()
+        }
+    }
+}
