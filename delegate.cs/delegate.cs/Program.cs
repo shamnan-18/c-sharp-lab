8a1fe514a701c9cc8b15cc7cc1024c8f34096147
shamnan-18 <ahmadshamnan575@gmail.com>
2025-10-10 10:51:14 +0530

    initial commit

---
+++ delegate.cs/delegate.cs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+delegate void greet();
+
+class program
+{
+
+    static void sayhello()
+    {
+        Console.WriteLine("hello good afternoon students!");
+    }
+
+
+    static void Main()
+    {
+        greet g = sayhello;
+        
+        g();
+    }
+}
