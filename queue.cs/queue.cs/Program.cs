a94f42e84c1e6d7c60689fe5884de24ecc6d0433
shamnan-18 <ahmadshamnan575@gmail.com>
2025-10-10 10:37:18 +0530

    initial commit

---
+++ queue.cs/queue.cs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+class Program
+{
+    public static void Show()
+    {
+        Queue<string> queue = new Queue<string>();
+        queue.Enqueue("sham");
+        queue.Enqueue("Charlie");
+        queue.Enqueue("afra");
+        queue.Enqueue("Bob");
+        queue.Dequeue();
+
+        foreach (string name in queue)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
+
+class MyClass
+{
+    static void Main()
+    {
+        Program.Show();
+    }
+
+}
